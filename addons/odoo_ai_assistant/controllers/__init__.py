b63131d25d60acecaf4ce18584b9b8817fe7a86c
Kiril Ferrer <CPX001@proton.me>
2026-08-25 01:14:53 +0200

    refactor(batch): load shared agent authority before batch routes

--- addons/odoo_ai_assistant/controllers/__init__.py
+++ addons/odoo_ai_assistant/controllers/__init__.py
@@ -1,4 +1,4 @@
+from . import internal_tools as internal_tools
 from . import batch_tools as batch_tools
 from . import browser_bridge as browser_bridge
 from . import chat_bridge as chat_bridge
-from . import internal_tools as internal_tools
