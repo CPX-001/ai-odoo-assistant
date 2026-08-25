2fb009bc1eeb7367e961b9d2497139fb15a93ae7
Kiril Ferrer <CPX001@proton.me>
2026-08-25 14:38:13 +0200

    feat(chat): load history action controller

--- addons/odoo_ai_assistant/controllers/__init__.py
+++ addons/odoo_ai_assistant/controllers/__init__.py
@@ -2,3 +2,4 @@ from . import internal_tools as internal_tools
 from . import batch_tools as batch_tools
 from . import browser_bridge as browser_bridge
 from . import chat_bridge as chat_bridge
+from . import chat_history_actions as chat_history_actions
