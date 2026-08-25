d73f30b5d0f067f940b5f988961dabaf20f29a79
Kiril Ferrer <CPX001@proton.me>
2026-08-25 19:00:10 +0200

    feat(chat): load browser streaming bridge

--- addons/odoo_ai_assistant/models/__init__.py
+++ addons/odoo_ai_assistant/models/__init__.py
@@ -3,6 +3,7 @@ from . import assistant_bridge as assistant_bridge
 from . import assistant_chat_bridge as assistant_chat_bridge
 from . import assistant_chat_failures as assistant_chat_failures
 from . import assistant_chat_history_actions as assistant_chat_history_actions
+from . import assistant_chat_stream as assistant_chat_stream
 from . import assistant_diagnostics as assistant_diagnostics
 from . import assistant_diagnostics_matrix as assistant_diagnostics_matrix
 from . import assistant_maintenance as assistant_maintenance
