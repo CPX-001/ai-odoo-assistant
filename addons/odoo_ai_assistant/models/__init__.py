462a52e2f13d345952b1c01cdc458850e1d0d43a
Kiril Ferrer <CPX001@proton.me>
2026-08-25 14:38:07 +0200

    feat(chat): load history action bridge

--- addons/odoo_ai_assistant/models/__init__.py
+++ addons/odoo_ai_assistant/models/__init__.py
@@ -1,6 +1,7 @@
 from . import action_execution as action_execution
 from . import assistant_bridge as assistant_bridge
 from . import assistant_chat_bridge as assistant_chat_bridge
+from . import assistant_chat_history_actions as assistant_chat_history_actions
 from . import assistant_diagnostics as assistant_diagnostics
 from . import assistant_diagnostics_matrix as assistant_diagnostics_matrix
 from . import assistant_maintenance as assistant_maintenance
