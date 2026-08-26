9466104318f9598ab8605b6107e8b1fe1cd0622f
Kiril Ferrer <CPX001@proton.me>
2026-08-26 12:39:05 +0200

    refactor(service): remove retired query and action workflows

--- service/src/odoo_ai/application/__init__.py
+++ service/src/odoo_ai/application/__init__.py
@@ -1,31 +1,5 @@
-"""Deterministic Assistant Service application workflows."""
+"""Application workflows still hosted by the temporary source/retrieval service."""
 
-from odoo_ai.application.action_approval import (
-    ActionApprovalError,
-    ActionApprovalService,
-)
-from odoo_ai.application.action_command import ActionCommandService
-from odoo_ai.application.action_execution import (
-    ActionExecutionError,
-    ActionExecutionService,
-)
-from odoo_ai.application.action_policy import (
-    ACTION_POLICY_REVISION,
-    ActionPolicy,
-    ActionPolicyError,
-    action_payload_fingerprint,
-    canonical_action_payload_bytes,
-)
-from odoo_ai.application.action_preview import (
-    ActionCreatePreviewResult,
-    ActionCreatePreviewService,
-    ActionPreviewError,
-    ActionPreviewResult,
-    ActionPreviewService,
-    BusinessActionPreviewResult,
-    BusinessActionPreviewService,
-)
-from odoo_ai.application.action_workflow import ActionService, ActionTurnError
 from odoo_ai.application.context_read import (
     ContextReadError,
     ContextReadService,
@@ -38,11 +12,6 @@ from odoo_ai.application.effective_schema import (
     EffectiveSchemaResult,
     EffectiveSchemaService,
 )
-from odoo_ai.application.effective_write_schema import (
-    EffectiveWriteSchemaError,
-    EffectiveWriteSchemaResult,
-    EffectiveWriteSchemaService,
-)
 from odoo_ai.application.explain import ExplainService, ExplainTurnError
 from odoo_ai.application.how_to import HowToService, HowToTurnError
 from odoo_ai.application.navigation import (
@@ -50,32 +19,8 @@ from odoo_ai.application.navigation import (
     NavigationService,
     NavigationServiceError,
 )
-from odoo_ai.application.query_primitives import (
-    AggregateRecordsExecution,
-    QueryPrimitiveError,
-    QueryPrimitiveService,
-    QueryRecordsExecution,
-)
-from odoo_ai.application.query_workflow import QueryService, QueryTurnError
 
 __all__ = [
-    "ACTION_POLICY_REVISION",
-    "ActionApprovalError",
-    "ActionApprovalService",
-    "ActionCommandService",
-    "ActionExecutionError",
-    "ActionExecutionService",
-    "ActionPolicy",
-    "ActionPolicyError",
-    "ActionCreatePreviewResult",
-    "ActionCreatePreviewService",
-    "ActionPreviewError",
-    "ActionPreviewResult",
-    "ActionPreviewService",
-    "BusinessActionPreviewResult",
-    "BusinessActionPreviewService",
-    "ActionService",
-    "ActionTurnError",
     "ContextReadError",
     "ContextReadService",
     "DiagnosticsError",
@@ -84,9 +29,6 @@ __all__ = [
     "EffectiveSchemaPolicy",
     "EffectiveSchemaResult",
     "EffectiveSchemaService",
-    "EffectiveWriteSchemaError",
-    "EffectiveWriteSchemaResult",
-    "EffectiveWriteSchemaService",
     "ExplainService",
     "ExplainTurnError",
     "HowToService",
@@ -94,13 +36,5 @@ __all__ = [
     "NavigationResult",
     "NavigationService",
     "NavigationServiceError",
-    "AggregateRecordsExecution",
-    "QueryPrimitiveError",
-    "QueryPrimitiveService",
-    "QueryRecordsExecution",
-    "QueryService",
-    "QueryTurnError",
     "TraceEventData",
-    "action_payload_fingerprint",
-    "canonical_action_payload_bytes",
 ]
