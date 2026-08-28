a05236686cea97552d450e1190244f5032fd4f44
Kiril Ferrer <CPX001@proton.me>
2026-08-28 11:12:52 +0200

    feat(activity): load live projection controller

--- addons/odoo_ai_assistant/controllers/__init__.py
+++ addons/odoo_ai_assistant/controllers/__init__.py
@@ -2,3 +2,4 @@ from . import chat_bridge as chat_bridge
 from . import chat_history_actions as chat_history_actions
 from . import internal_tools as internal_tools
 from . import turn_runtime as turn_runtime
+from . import turn_live as turn_live
