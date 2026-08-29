1352499cea7f955a07335e868a6b2ad47f1a6cc2
Kiril Ferrer <CPX001@proton.me>
2026-08-30 01:24:56 +0200

    fix(p5.8): register turn control projection

--- addons/odoo_ai_assistant/models/__init__.py
+++ addons/odoo_ai_assistant/models/__init__.py
@@ -17,6 +17,7 @@ from . import turn_reasoning_summary as turn_reasoning_summary
 from . import turn_working_transcript as turn_working_transcript
 from . import embedded_runtime_host_loop as embedded_runtime_host_loop
 from . import turn_control as turn_control
+from . import turn_control_projection as turn_control_projection
 from . import user_preferences as user_preferences
 from . import activity_preferences as activity_preferences
 from . import public_references as public_references
