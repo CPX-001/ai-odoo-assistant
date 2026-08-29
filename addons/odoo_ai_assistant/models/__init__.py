94a8d53d405ddc98216a4dc202d2a24cde6c7a9c
Kiril Ferrer <CPX001@proton.me>
2026-08-29 15:20:22 +0200

    feat: load reasoning preference extension

--- addons/odoo_ai_assistant/models/__init__.py
+++ addons/odoo_ai_assistant/models/__init__.py
@@ -17,4 +17,5 @@ from . import turn_live_event as turn_live_event
 from . import turn_working_transcript as turn_working_transcript
 from . import embedded_runtime_host_loop as embedded_runtime_host_loop
 from . import user_preferences as user_preferences
+from . import reasoning_preferences as reasoning_preferences
 from . import conversation_context as conversation_context
