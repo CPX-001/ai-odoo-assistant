9c320732c68e024a2104c499b3100e8519bbc398
Kiril Ferrer <CPX001@proton.me>
2026-08-29 21:16:46 +0200

    feat(p5.8d): register public reference route

--- addons/odoo_ai_assistant/controllers/__init__.py
+++ addons/odoo_ai_assistant/controllers/__init__.py
@@ -2,5 +2,6 @@ from . import activity_preferences as activity_preferences
 from . import chat_bridge as chat_bridge
 from . import chat_history_actions as chat_history_actions
 from . import internal_tools as internal_tools
+from . import public_references as public_references
 from . import turn_runtime as turn_runtime
 from . import turn_live as turn_live
