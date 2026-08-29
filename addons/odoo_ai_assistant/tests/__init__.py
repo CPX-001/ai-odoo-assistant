ae680f1804e47bb610ba4032737ac3d4d168b497
Kiril Ferrer <CPX001@proton.me>
2026-08-29 21:12:29 +0200

    test(p5.8c): register reasoning summary projection tests

--- addons/odoo_ai_assistant/tests/__init__.py
+++ addons/odoo_ai_assistant/tests/__init__.py
@@ -26,6 +26,7 @@ from . import (
 )
 from . import test_phase4_live_projection as test_phase4_live_projection
 from . import test_post_effect_reasoning as test_post_effect_reasoning
+from . import test_reasoning_summary_projection as test_reasoning_summary_projection
 from . import test_runtime_account as test_runtime_account
 from . import test_runtime_account_gate as test_runtime_account_gate
 from . import test_turn_failure as test_turn_failure
