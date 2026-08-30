953278b6ae71ed9cc4093f4d6584e153ba961113
Kiril Ferrer <CPX001@proton.me>
2026-08-30 02:04:18 +0200

    feat(p5.8): register persisted navigation references

--- addons/odoo_ai_assistant/models/__init__.py
+++ addons/odoo_ai_assistant/models/__init__.py
@@ -25,6 +25,7 @@ from . import turn_control_interrupt_persistence as turn_control_interrupt_persi
 from . import user_preferences as user_preferences
 from . import activity_preferences as activity_preferences
 from . import public_references as public_references
+from . import turn_navigation as turn_navigation
 from . import reasoning_preferences as reasoning_preferences
 from . import conversation_context as conversation_context
 from . import conversation_preferences as conversation_preferences
