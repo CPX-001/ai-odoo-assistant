dc7d9e6d48fbd44497b1f5eb7fbd98ce75e3894c
Kiril Ferrer <CPX001@proton.me>
2026-08-31 10:37:06 +0200

    feat(chat): move Plan into composer add menu

--- addons/odoo_ai_assistant/static/src/components/assistant_planning/assistant_planning.js
+++ addons/odoo_ai_assistant/static/src/components/assistant_planning/assistant_planning.js
@@ -1,29 +1,18 @@
 /** @odoo-module **/
 
-import { _t } from "@web/core/l10n/translation";
 import { patch } from "@web/core/utils/patch";
 import { AssistantPanel } from "@odoo_ai_assistant/components/assistant_panel/assistant_panel";
 
-const MODE_LABELS = {
-    adaptive: _t("Directo"),
-    deliberate: _t("Plan"),
-};
-
-const MODE_ICONS = {
-    adaptive: "fa-bolt",
-    deliberate: "fa-list-ol",
-};
+export function planningToggleTarget(mode) {
+    return mode === "deliberate" ? "adaptive" : "deliberate";
+}
 
 patch(AssistantPanel.prototype, {
-    get planningModeLabel() {
-        return MODE_LABELS[this.state.planningMode] || MODE_LABELS.adaptive;
+    get planModeActive() {
+        return this.state.planningMode === "deliberate";
     },
 
-    get planningModeIconClass() {
-        return MODE_ICONS[this.state.planningMode] || MODE_ICONS.adaptive;
-    },
-
-    async selectPlanningMode(mode) {
-        await this.panel.setPlanningMode(mode);
+    async togglePlanMode() {
+        await this.panel.setPlanningMode(planningToggleTarget(this.state.planningMode));
     },
 });
