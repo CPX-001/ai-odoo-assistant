98e086b28e584cb374caf2b5b6308c70cd8297f5
Kiril Ferrer <CPX001@proton.me>
2026-09-03 17:22:14 +0200

    feat(p11): register import cleanup repair model

--- addons/odoo_ai_assistant/models/__init__.py
+++ addons/odoo_ai_assistant/models/__init__.py
@@ -4,6 +4,7 @@ from . import chat_policy as chat_policy
 from . import chat_preferences as chat_preferences
 from . import chat_storage as chat_storage
 from . import data_import as data_import
+from . import data_import_repair as data_import_repair
 from . import knowledge as knowledge
 from . import knowledge_attachment_link as knowledge_attachment_link
 from . import knowledge_fts_index as knowledge_fts_index
